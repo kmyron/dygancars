f5d1c8a8416dde7cc7bc372f95b97799ce2d9b9c
kmyron <myronkv@gmail.com>
2015-05-01 15:51:38 +0300

    jquery changes2

--- js/jquery.js
+++ js/jquery.js
@@ -7,4 +7,38 @@
     $(".item").click(function(){
         $(this).toggleClass("grey");
     });
+    Interdimensional.charge({
+
+  /**
+   * Pixels per difference between tilts
+   * @type {Number}
+   */
+  PPD: 0.8,
+
+  /**
+   * Minimum difference between tilts
+   * @type {Number}
+   */
+  insensitivity: 5,
+
+  /**
+   * Use the control or not
+   * @type {Boolean}
+   */
+  useControl: true,
+
+  /**
+   * Interdimensional control
+   *
+   * If null - default control will be used
+   * control: document.getElementById('myControl')
+   *
+   * @type {HTMLElement|null}
+   */
+  control: null  
+});
+            $('.modal').addEventListener('interdimensional:charge', function() {
+  Interdimensional.jump();
+}, false);
+
      });    
